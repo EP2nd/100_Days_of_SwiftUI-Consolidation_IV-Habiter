6bd52e863091490282464d2ccd6a139de4d2f5de
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-19 21:19:16 +0100

    Initial Commit

---
+++ Habiter/HabiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiterApp.swift
+//  Habiter
+//
+//  Created by Edwin Przeźwiecki Jr. on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
